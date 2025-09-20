46a34073f39c3a203fb01311bee6ca432d5d53aa
qalmurri <86642474+qalmurri@users.noreply.github.com>
2025-09-21 01:24:32 +0700

    Delete src/features/public/gi/tos/components directory

--- src/features/public/gi/tos/components/atoms/Paragraph.tsx
+++
@@ -1,8 +0,0 @@
-type ParagraphProps = {
-  children: React.ReactNode;
-  className?: string;
-};
-
-export default function Paragraph({ children, className }: ParagraphProps) {
-  return <p className={`mb-4 ${className || ""}`}>{children}</p>;
-}
