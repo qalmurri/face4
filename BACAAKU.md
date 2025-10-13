8c2d0a5fec097af9572a1de7c7de680dc68755ea
qalmurri2 <blog.qalmurri@gmail.com>
2025-10-13 22:58:58 +0700

    modified:   BACAAKU.md 	deleted:    api/auth/api/accounts/migrations/0001_initial.py 	deleted:    api/auth/api/authentication/migrations/0001_initial.py 	deleted:    api/auth/api/db.sqlite3 	deleted:    api/auth/api/request/migrations/0001_initial.py 	modified:   package-lock.json 	modified:   package.json 	new file:   src/components/ErrorBoundary.tsx 	deleted:    src/contexts/index.tsx 	modified:   src/main.tsx 	modified:   src/services/axiosInstance.tsx

--- BACAAKU.md
+++ BACAAKU.md
@@ -1,4 +1,4 @@
 npm create vite@latest MyProjectName > Framework: react > variant: typescript + SWC
-npm install tailwindcss @tailwindcss/vite react-router-dom axios clsx react-icons
+npm install tailwindcss @tailwindcss/vite react-router-dom axios clsx react-icons react-hot-toast
 
 https://tailwindcss.com/docs/installation/using-vite
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { AuthTokenProvider } from "./contexts/TokenContext.tsx";
 import { AuthStatusProvider } from "./contexts/StatusContext.tsx";
+import ErrorBoundary from "./components/ErrorBoundary.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthTokenProvider>
-      <AuthStatusProvider>
-        <App />
-      </AuthStatusProvider>
-    </AuthTokenProvider>
+    <ErrorBoundary>
+      <AuthTokenProvider>
+        <AuthStatusProvider>
+          <App />
+        </AuthStatusProvider>
+      </AuthTokenProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
