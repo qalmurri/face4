7397bdace9880baeb05ba99f6d5743c4ec7be6f4
qalmurri <86642474+qalmurri@users.noreply.github.com>
2025-10-22 08:09:39 +0700

    Delete src/components directory

--- src/components/atoms/brands/Brand.tsx
+++
@@ -1,7 +0,0 @@
-interface BrandProps {
-    children: React.ReactNode;
-}
-
-export function Brand({ children }: BrandProps) {
-    return <div className="text-lg font-bold">{children}</div>;
-}
--- src/components/atoms/icons/Icon.tsx
+++
@@ -1,7 +0,0 @@
-interface IconProps {
-    children: React.ReactNode; // bisa pakai lucide-react, fontawesome dll
-}
-
-export function Icon({ children }: IconProps) {
-    return <span className="inline-flex items-center">{children}</span>;
-}
