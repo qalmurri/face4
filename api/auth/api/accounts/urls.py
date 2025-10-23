9071de3d661157b6d025f41ce28d7bbd221c82ee
qalmurri2 <blog.qalmurri@gmail.com>
2025-10-23 14:41:22 +0700

    modified:   api/auth/api/accounts/urls.py

--- api/auth/api/accounts/urls.py
+++ api/auth/api/accounts/urls.py
@@ -5,13 +5,10 @@ from .views import UserPhoneView, UserPhoneUpdateView, UserAddressView, UserAddr
 urlpatterns = [
     path("phone/", UserPhoneView.as_view(), name="user-phone"),
     path("phone/update/", UserPhoneUpdateView.as_view(), name="user-phone-update"),
-
     path("address/", UserAddressView.as_view(), name="user-address"),
     path("address/update/", UserAddressUpdateView.as_view(), name="user-address-update"),
-
     path("preference/", UserPreferenceView.as_view(), name="user-preference"),
     path("preference/update/", UserPreferenceUpdateView.as_view(), name="user-preference-update"),
-
     path("display/", UserDisplayView.as_view(), name="user-display"),
     path("display/update/", UserDisplayUpdateView.as_view(), name="user-display-update"),
 ]
