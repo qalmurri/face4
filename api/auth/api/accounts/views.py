6fce848ab7552a52779f0c7f2d318402359e8867
qalmurri2 <blog.qalmurri@gmail.com>
2025-10-24 09:15:44 +0700

    modified:   api/auth/api/accounts/views.py

--- api/auth/api/accounts/views.py
+++ api/auth/api/accounts/views.py
@@ -2,7 +2,7 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import PhoneSerializer, PreferenceSerializer, DisplaySerializer, AddressSerializer
-from .models import Phone
+from .models import Phone, Preference, Display, Address
 from authentication.models import Profile
 
 
