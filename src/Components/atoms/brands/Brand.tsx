72b5e60894c5a18693b570c11bc6c7862fb8bf93
qalmurri <86642474+qalmurri@users.noreply.github.com>
2025-10-25 08:07:20 +0700

    Delete src/Components/atoms directory

--- src/Components/atoms/brands/Brand.tsx
+++
@@ -1,9 +0,0 @@
-interface BrandProps {
-    children: React.ReactNode;
-}
-
-export function Brand({ children }: BrandProps) {
-    return <div className="text-lg font-bold">{children}</div>;
-}
-
-//test
--- src/Components/atoms/typography/Paragraph.tsx
+++
@@ -1,8 +0,0 @@
-type ParagraphProps = {
-  children: React.ReactNode;
-  className?: string;
-};
-
-export function Paragraph({ children, className }: ParagraphProps) {
-  return <p className={`mb-4 ${className || ""}`}>{children}</p>;
-}
