19b2fa0c30a3f07226968da6bf868b688d5d9ead
qalmurri <blog.qalmurri@gmail.com>
2025-10-28 20:07:46 +0700

    deleted:    api/image/README.md 	deleted:    api/image/api/accounts/__init__.py 	deleted:    api/image/api/accounts/apps.py 	deleted:    api/image/api/accounts/migrations/__init__.py 	deleted:    api/image/api/accounts/models.py 	deleted:    api/image/api/accounts/serializers.py 	deleted:    api/image/api/accounts/urls.py 	deleted:    api/image/api/accounts/views.py 	deleted:    api/image/api/api/__init__.py 	deleted:    api/image/api/api/asgi.py 	deleted:    api/image/api/api/settings.py 	deleted:    api/image/api/api/urls.py 	deleted:    api/image/api/api/wsgi.py 	deleted:    api/image/api/authentication/__init__.py 	deleted:    api/image/api/authentication/apps.py 	deleted:    api/image/api/authentication/migrations/__init__.py 	deleted:    api/image/api/authentication/models.py 	deleted:    api/image/api/authentication/serializers.py 	deleted:    api/image/api/authentication/urls.py 	deleted:    api/image/api/authentication/views.py 	deleted:    api/image/api/manage.py 	deleted:    api/image/api/request/__init__.py 	deleted:    api/image/api/request/apps.py 	deleted:    api/image/api/request/migrations/__init__.py 	deleted:    api/image/api/request/models.py 	deleted:    api/image/api/request/serializers.py 	deleted:    api/image/api/request/urls.py 	deleted:    api/image/api/request/utils.py 	deleted:    api/image/api/request/views.py 	deleted:    api/image/api/utils/exceptions.py 	deleted:    api/image/api/utils/mask.py

--- api/image/api/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-
-urlpatterns = [
-     path('auth/', include('authentication.urls')),
-     path('req/', include('request.urls')),
-     path('acc/', include('accounts.urls')),
-]
